81ad1c061cabbb3c97ad8c6132676715272c7c39
Marcelo Junio <marcelo.trt@gmail.com>
2023-12-27 16:13:00 -0300

    foi criado mais um elemento referente ao id do chamado para ser salvo na criação de um chamado

--- registra_chamado.php
+++ registra_chamado.php
@@ -2,10 +2,36 @@
 
     session_start();
 
+    $arquivo = fopen('arquivo.hd','r');
+
+    while(!feof($arquivo)) {
+    $registro = fgets($arquivo); //recupera os dados da linha
+    $chamados[] = $registro;  
+
+}
+
+foreach($chamados as $chamado) {
+$dado_chamado = explode('#',$chamado);
+
+$numero_chamado = count($chamados) + 1;
+
+if($dado_chamado[4] == $numero_chamado){
+   $numero_chamado = count($chamados) + 1;
+ 
+}
+
+}
+
+
+    fclose($arquivo);
+
+    $numero_chamado = count($chamados) + 1;  
+ 
+
     $titulo = str_replace('#','-',$_POST['titulo']);
     $categoria = str_replace('#','-',$_POST['categoria']);
     $descricao = str_replace('#','-',$_POST['descricao']);
-    $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+    $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . '#'. $numero_chamado . PHP_EOL;
 
     $arquivo = fopen("arquivo.hd","a");
 
